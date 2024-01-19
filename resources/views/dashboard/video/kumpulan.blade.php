8b02b288bbed1e3d3b28cbe1bd00c66ad4e80e3b
unknown <agihananmunawar@gmail.com>
2024-01-20 00:44:44 +0700

    awal

---
+++ resources/views/dashboard/video/kumpulan.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.Dashboard.app')
+@section('content')
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-body shadow bg-warning rounded-4">
+                <div class="row">
+                    <div class="">
+                       <div class="fw-bold text-center text-white mb-3" style="font-size: 30px;">KUMPULAN VIDEO KUTAHU</div>
+                    </div>
+                    <div class="col-12 d-flex justify-content-between">
+                        <div class="col-4 me-2">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/HgXnfKsdcu0?si=sRfLjbJUTrFlaeIp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <p class=" text-center text-white">Judul</p>
+                        </div>
+                        <div class="col-4 ">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/BRxMbaHnFlA?si=kzA3_pEdlkIPg-gJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <p class="text-center text-white">Judul</p>
+                        </div>
+                        <div class="col-4 ms-2">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/sFWPdPAO2FU?si=QsS68TAWLFHC1Y2P" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <p class="text-center text-white">Judul</p>
+                        </div>
+                    </div>
+                    <div class="col-12 d-flex justify-content-between">
+                        <div class="col-4 me-2">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/-8L7wTpjZik?si=iUqj3rwL1cKToGFG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <p class=" text-center text-white">Judul</p>
+                        </div>
+                        <div class="col-4 ">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/npcFJng7Dn0?si=uIL60euevoOo11Qe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <p class="text-center text-white">Judul</p>
+                        </div>
+                        <div class="col-4 ms-2">
+                            <iframe width="100%" height="200" src="https://www.youtube.com/embed/Uh-OxkYnLUc?si=Dk4QnhahEOhnj6dJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <p class="text-center text-white">Judul</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+@endsection
